f37df657a5090bdc13b1cc3add71228f8cc1d76f
Jessé Bruno Correia <jesse.bcorreia@gmail.com>
2025-08-16 13:31:49 -0300

    v.1.8

--- test/03-insert-equipe-tecnica.sql
+++ test/03-insert-equipe-tecnica.sql
@@ -1,19 +1,27 @@
 INSERT INTO
     usuario (email, senha, cargo, criado_em, endereco_id)
 VALUES
+    -- id: 2
     ('joao.analista@email.com', '123456789', 'ANALISTA', '2025-01-23 21:52:16', 4),
+    -- id: 3
     ('maria.analista@email.com', '987654321', 'ANALISTA', '2025-03-12 15:25:36', 5),
+    -- id: 4
     ('josicleison.pedagogo@email.com', '123456789', 'PEDAGOGO', '2025-05-22 12:32:23', 6),
+    -- id: 5
     ('luana.pedagoga@email.com', '987654321', 'PEDAGOGO', '2025-02-10 19:15:58', 7);
 
 INSERT INTO
     usuario_analista (usuario_id, nome, sobrenome, cpf, sexo)
 VALUES
+    -- usuario_id: 2
     (2, 'João', 'Analista da Silva', '861.534.872-92', 'M'),
+    -- usuario_id: 3
     (3, 'Maria', 'Pereira Analista da Costa', '845.269.456-15', 'F');
 
 INSERT INTO
     usuario_pedagogo (usuario_id, nome, sobrenome, cpf, sexo)
 VALUES
+    -- usuario_id: 4
     (4, 'Josicleison', 'o Pedagogo', '563.534.856-92', 'M'),
+    -- usuario_id: 5
     (5, 'Luana', 'Andrade Pedagoga Santos', '379.568.339-56', 'F');
